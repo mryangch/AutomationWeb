06f8ad2295c446808f6aa521a5eb2aa0f38e3f37
Chunhai Yang <mryangch001@gmail.com>
2018-12-18 13:50:07 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ node {
                 echo 'Workspace:'+"${env.WORKSPACE}"
                 bat 'mkdir archive'
                 bat 'echo test > archive/test.txt'
-                //zip zipFile: 'test.zip', archive: false, dir: 'archive'
+                zip zipFile: 'package.zip', archive: false, dir: 'archive'
                 //archiveArtifacts artifacts: 'test.zip', fingerprint: true
            
         }
