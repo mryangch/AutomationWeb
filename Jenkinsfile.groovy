91da2a1bf7e9bc1286bacee909156fac6b490e70
Chunhai Yang <mryangch001@gmail.com>
2018-12-18 11:38:56 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,11 +8,16 @@ node {
         
 	bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /t:Restore"
 	bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
+	
     }
-    stage('Archive') {
-        echo 'Run test cases...'
-        //archiveArtifacts 'AzureWebOAuth2/bin/Release'
-    }
+   stage ('push artifact') {
+            steps {
+                sh 'mkdir archive'
+                sh 'echo test > archive/test.txt'
+                zip zipFile: 'test.zip', archive: false, dir: 'archive'
+                archiveArtifacts artifacts: 'test.zip', fingerprint: true
+            }
+        }
     stage('Deploy') {
         echo 'Deploy package...'
     }
