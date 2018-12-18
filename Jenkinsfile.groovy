f949e9e063b483f5ab5aefa30bad73a4cd26a220
Chunhai Yang <mryangch001@gmail.com>
2018-12-18 16:20:29 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,42 +1,27 @@
 node {
-	environment {
-		
-     //PackageName = "Package_"+${currentBuild.startTimeInMillis}+".zip"
-     
-    // BinaryFolder=""
-   }
     stage('Checkout') {
         checkout scm
         echo 'Downloading codes...'
     }
     stage('Build') {
         echo 'Build project...'
-        
-	//bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /t:Restore"
-	//bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
-	
-    }
-   stage('ZIP Artifact') {     
-                //echo 'Workspace:'+"${env.WORKSPACE}"
-	   	//bat 'dir'
-	   //echo "TimeStamp: ${currentBuild.startTimeInMillis}"
-	   def timeStamp = Calendar.getInstance().getTime().format('YYYYMMdd-hhmmss',TimeZone.getTimeZone('Asia/Singapore'))
-	   echo 'time stamp:'+"${timeStamp}"
-	   def packageName = 'Package_'+"${timeStamp}"+'.zip'
-	    echo 'Package Name:'+"${packageName}"
+        bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /t:Restore"
+        bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
 
-		dir ('Archive') {
-			zip zipFile: "${packageName}", archive: false, dir: '../AutomationWeb/bin/Release/netcoreapp2.1/publish'
-		  archiveArtifacts artifacts: "${packageName}", fingerprint: true
-		  bat 'del ' +"${packageName}"
-		}
-		//bat 'dir'
-                
-                //bat 'echo test > archive/test.txt'
-                //zip zipFile: 'Archive/package.zip', archive: false, dir: 'AutomationWeb/bin/Release/netcoreapp2.1/publish'
-                //archiveArtifacts artifacts: 'test.zip', fingerprint: true
-           
+    }
+    stage('ZIP Artifact') {
+        //echo 'Workspace:'+"${env.WORKSPACE}"
+        def timeStamp = Calendar.getInstance().getTime().format('YYYYMMddhhmmss', TimeZone.getTimeZone('Singapore'))
+        echo 'time stamp:' + "${timeStamp}"
+        def packageName = 'Package_' + "${timeStamp}" + '.zip'
+        echo 'Package Name:' + "${packageName}"
+
+        dir('Archive') {
+            zip zipFile: "${packageName}", archive: false, dir: '../AutomationWeb/bin/Release/netcoreapp2.1/publish'
+            archiveArtifacts artifacts: "${packageName}", fingerprint: true
+            bat 'del ' + "${packageName}"
         }
+    }
     stage('Deploy') {
         echo 'Deploy package...'
     }
