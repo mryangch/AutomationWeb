9a20e7ef270eb1db1b0942bc5c966c698d6559a5
ChunhaiYang <mryangch001@gmail.com>
2018-12-18 16:43:11 +0800

    Define global variable

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,20 @@
 node {
+    def timeStamp = Calendar.getInstance().getTime().format('YYYYMMddHHmmss', TimeZone.getTimeZone('Singapore'))
+    //echo 'time stamp:' + "${timeStamp}"
+    def packageName = 'Package_' + "${timeStamp}" + '.zip'
+    
     stage('Checkout') {
         checkout scm
         echo 'Downloading codes...'
     }
     stage('Build') {
         echo 'Build project...'
-        bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /t:Restore"
-        bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
-
+        //bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /t:Restore"
+        //bat "\"${tool 'msbuild15.0'}\" AutomationWeb.sln /p:Configuration=Release /p:Platform=\"Any CPU\""
     }
     stage('ZIP Artifact') {
         //echo 'Workspace:'+"${env.WORKSPACE}"
-        def timeStamp = Calendar.getInstance().getTime().format('YYYYMMddHHmmss', TimeZone.getTimeZone('Singapore'))
-        echo 'time stamp:' + "${timeStamp}"
-        def packageName = 'Package_' + "${timeStamp}" + '.zip'
         echo 'Package Name:' + "${packageName}"
-
         dir('Archive') {
             zip zipFile: "${packageName}", archive: false, dir: '../AutomationWeb/bin/Release/netcoreapp2.1/publish'
             archiveArtifacts artifacts: "${packageName}", fingerprint: true
