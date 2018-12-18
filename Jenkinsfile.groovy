e39ec799444ed51e3bad443641a06c8c34c37253
Chunhai Yang <mryangch001@gmail.com>
2018-12-18 15:11:11 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,11 +12,11 @@ node {
     }
    stage('ZIP Artifact') {     
                 //echo 'Workspace:'+"${env.WORKSPACE}"
-	   	bat 'dir'
+	   	//bat 'dir'
 		dir ('Archive') {
-	          zip zipFile: 'Archive/package.zip', archive: false, dir: 'AutomationWeb/bin/Release/netcoreapp2.1/publish'
+	          zip zipFile: 'Package.zip', archive: false, dir: '../AutomationWeb/bin/Release/netcoreapp2.1/publish'
 		}
-		bat 'dir'
+		//bat 'dir'
                 
                 //bat 'echo test > archive/test.txt'
                 //zip zipFile: 'Archive/package.zip', archive: false, dir: 'AutomationWeb/bin/Release/netcoreapp2.1/publish'
